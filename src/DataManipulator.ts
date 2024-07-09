4e4e222e43aad3590fcff6f9a806c768cd6bea89
Shanmugesh <149296635+shanmugesh-developers@users.noreply.github.com>
2024-07-09 14:10:32 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -8,13 +8,23 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC=(serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
+        const priceDEF=(serverRespond[1].top_ask.price+serverRespond[1].top_bid.price)/2;
+        const radio=priceABC/priceDEF;
+    const upperBound =1+0.05;
+    const lower=1-0.05;
     return serverResponds.map((el: any) => {
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc:priceABC,
+        price_def:priceDEF,
+        radio,
+        timestamp:serverRespond[0].timestamp>serverRespond[1].timestamp?
+          upper_bound:upperBound,
+          lower_bound:lowerBound,
+          trigger_alert:(radio>upperBOUND||radio<lowerBound)?radio:undefined,
+        
       };
-    })
+    }
   }
 }
